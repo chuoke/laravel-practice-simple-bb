e4b669e747214cd8884e9e7786d85645ee7c0094
chuoke <chuokechungyoung@gmail.com>
2019-05-03 18:13:59 +0800

    upgrade jquery to ^3.4.0

--- public/js/app.js
+++ public/js/app.js
@@ -36684,8 +36684,8 @@ if (token) {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! E:\HomeWork\publicdev\laravels\simple-bb\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! E:\HomeWork\publicdev\laravels\simple-bb\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\Codes\homes\laravels\laravel-practice-simple-bb\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\Codes\homes\laravels\laravel-practice-simple-bb\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
