9e96b85825c3b5342f9f9aa859dc7f2f2185ebaa
chuoke <chuokechungyoung@gmail.com>
2019-05-05 23:52:01 +0800

    话题回复基础和数据填充

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -18,6 +18,11 @@ class Topic extends Model
         return $this->belongsTo(User::class, 'user_id', 'id');
     }
 
+    public function replies()
+    {
+        return $this->hasMany(Reply::class, 'topic_id', 'id');
+    }
+
     public function scopeWithOrder($query, $order)
     {
         switch ($order) {
--- app/Models/User.php
+++ app/Models/User.php
@@ -62,6 +62,11 @@ class User extends Authenticatable implements MustVerifyEmailContract
         return $this->hasMany(Topic::class, 'user_id', 'id');
     }
 
+    public function replies()
+    {
+        return $this->hasMany(Reply::class, 'user_id', 'id');
+    }
+
     public function isAuthorOf(Model $model)
     {
         return $model->user_id === $this->id;
